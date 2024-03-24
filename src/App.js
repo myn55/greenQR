a7d4adc31fc17f9674c4b2b8d2672581387f584e
Muhammad Nabil <coronhaus@gmail.com>
2024-03-24 15:40:23 -0400

    Square one

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-import { TitleContainer } from './components/TitleContainer';
-import { SearchBarContainer } from './components/SearchBarContainer';
-import { SearchResultsContainer } from './components/SearchResultsContainer';
-import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [searchResults, setSearchResults] = useState([])
-
   return (
     <div className='App'>
 
