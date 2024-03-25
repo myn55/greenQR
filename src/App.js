ea2cb2dfe5101bde19c96665bb29f0e9097812ea
Muhammad Nabil <coronhaus@gmail.com>
2024-03-25 10:37:07 -0400

    Grid outline

--- src/App.js
+++ src/App.js
@@ -3,7 +3,27 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
+      <div className='grid-container'>
+        <div className='grid-header page-header'>
+          <span className='page-title'>header</span>
+        </div>
+        
+        <div className='grid-nav'>
+          <h1>navbar</h1>
+        </div>
+        
+        <div className='grid-side'>
+          <h1>sidebar</h1>
+        </div>
 
+        <div className='grid-main'>
+          <h1>main</h1>
+        </div>
+
+        <div className='grid-footer'>
+          <h1>footer</h1>
+        </div>
+      </div>
     </div>
   );
 }
